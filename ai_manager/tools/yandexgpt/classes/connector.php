c586ae8618163df7461a1d1e44d9098e2fb6f4bc
Artem Burashnikov <artem.burashnikov@gmail.com>
2025-11-09 11:27:02 +0300

    Refactor yagpt

--- ai_manager/tools/yandexgpt/classes/connector.php
+++ ai_manager/tools/yandexgpt/classes/connector.php
@@ -126,6 +126,11 @@ class connector extends \local_ai_manager\base_connector {
     }
 
     #[\Override]
+    public function has_customvalue3(): bool {
+        return true;
+    }
+
+    #[\Override]
     protected function get_headers(): array {
         $headers = parent::get_headers();
         
